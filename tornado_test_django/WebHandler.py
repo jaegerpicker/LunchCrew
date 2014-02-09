b50c1be5de1dee86be6c569e0600f18932eda5c1
Shawn Campbell <jaegerpicker@gmail.com>
2014-02-09 02:11:40 -0500

    Got the Webhandler completely async

--- tornado_test_django/WebHandler.py
+++ tornado_test_django/WebHandler.py
@@ -1,12 +1,23 @@
 """Tornado's main http handler """
 from tornado.web import RequestHandler
+from tornado.gen import coroutine
+from tornado.gen import Task
 from lunch_crew.models import PlaceToEat
 import datetime
 
 
 class MainHandler(RequestHandler):
     """ Handler Class """
+
+    @coroutine
     def get(self):
         """ Get method of the class """
-        pps = PlaceToEat.objects.all()[0]
+        a = AsyncDjango()
+        pps = yield Task(a.get_place_to_eat)
         self.write("Hello I'm from tornado " + pps.place_name)
+
+
+class AsyncDjango():
+    def get_place_to_eat(self, callback):
+        p = PlaceToEat.objects.all()[0]
+        callback(p)
